cc353e984385d14bb17b0d1377040209090c4193
egonzher <77987414+egonzher@users.noreply.github.com>
2021-05-04 15:48:49 +0200

    Subiendo un pequeño nuevos cambios

--- src/main/scala/com/sparkbyexamples/spark/rrhh/etl_datos_weci_consolid_wd.scala
+++ src/main/scala/com/sparkbyexamples/spark/rrhh/etl_datos_weci_consolid_wd.scala
@@ -28,35 +28,85 @@ object etl_datos_weci_consolid_wd {
     df_WeciConsolidWD.printSchema()
 
 /*
- |    |-- ns1:Person_Identification: struct (nullable = true)
- |    |    |-- ns1:National_Identifier: array (nullable = true)
- |    |    |    |-- element: struct (containsNull = true)
- |    |    |    |    |-- ns1:Country: string (nullable = true)
- |    |    |    |    |-- ns1:National_ID: string (nullable = true)
- |    |    |    |    |-- ns1:National_ID_Type: string (nullable = true)
- |    |    |-- ns1:Other_Identifier: array (nullable = true)
- |    |    |    |-- element: struct (containsNull = true)
- |    |    |    |    |-- ns1:Custom_ID_Type: string (nullable = true)
- |    |    |-- ns1:Passport: string (nullable = true)
- |    |    |-- ns1:Visa: string (nullable = true)
+ |    |-- ns1:Worker_Status: struct (nullable = true)
+ |    |    |-- ns1:Active: boolean (nullable = true)
+ |    |    |-- ns1:Active_Status_Date: string (nullable = true)
+ |    |    |-- ns1:Continuous_Service_Date: string (nullable = true)
+ |    |    |-- ns1:First_Day_of_Work: string (nullable = true)
+ |    |    |-- ns1:Hire_Date: string (nullable = true)
+ |    |    |-- ns1:Is_Rehire: boolean (nullable = true)
+ |    |    |-- ns1:Not_Returning: long (nullable = true)
+ |    |    |-- ns1:Original_Hire_Date: string (nullable = true)
+ |    |    |-- ns1:Regrettable_Termination: long (nullable = true)
+ |    |    |-- ns1:Return_Unknown: long (nullable = true)
+ |    |    |-- ns1:Secondary_Termination_Reason: string (nullable = true)
+ |    |    |-- ns1:Seniority_Date: string (nullable = true)
+ |    |    |-- ns1:Status: string (nullable = true)
+ |    |    |-- ns1:Terminated: boolean (nullable = true)
  */
 
     val df_WeciFinal = df_WeciConsolidWD
       .withColumn("natioId", explode(col("`ns:Employees`.`ns1:Person_Identification`.`ns1:National_Identifier`")))
-      //.withColumn("job_class", explode(col("`wd:Job_Classifications_group`")))
+      .withColumn("relatedPerson", explode(col("`ns:Employees`.`ns1:Related_Person`")))
       .selectExpr(
+        //Estos son los de color Blanco que no había dudas
         "`natioId`.`ns1:National_ID_Type` as NATIONAL_ID_TYPE_DESCR",
         "`ns:Employees`.`ns1:Personal`.`ns1:Date_of_Birth` as BIRTHDATE",
         "`ns:Employees`.`ns1:Personal`.`ns1:City_of_Birth` as BIRTHPLACE",
         "`ns:Employees`.`ns1:Personal`.`ns1:Country_of_Birth` as BIRTHCOUNTRY",
         "`natioId`.`ns1:National_ID` as NATIONALITY_ID",
-        //"`wd:Hire_Reason` as Hire_Reason",
-        //"`wd:Original_Hire_Date` as Original_Hire_Date",
-        //"`wd:dateOfBirth` as date_of_Birth",
-        //"`wd:City_of_Birth` as City_of_Birth",
-        //Este tuve que agregar el nivel inicial wd:Position_group pq o no era capaz de encontrar el .`wd:Home_Country`.`_Descriptor`
-        //"`wd:Position_group`.`wd:Home_Country`.`_Descriptor` as Home_Country"
+        "`ns:Employees`.`ns1:Personal`.`ns1:Nationality` as NATIONALITY_DESCR",
+        "`ns:Employees`.`ns1:Personal`.`ns1:Preferred_Language` as LANG_CD_DESCR",
+        "`ns:Employees`.`ns1:Personal`.`ns1:Gender` as GENDER_DESCR",
+        "`ns:Employees`.`ns1:Position`.`ns1:Job_Classification`.`ns1:Job_Classification_Description` as JOB_CLASSIFICATION_EFFDT",
+        "`ns:Employees`.`ns1:Collective_Agreement`.`ns1:Collective_Agreement` as COLLECTIVE_AGREEMENT_DESCR",
+        "`ns:Employees`.`ns1:Collective_Agreement`.`ns1:Collective_Agreement_Factor`.`ns1:Factor` as COLLECTIVE_AGREEMENT_FACTOR_DESCR",
+        "`ns:Employees`.`ns1:Position`.`ns1:Position_Time_Type` as JORNADA_EFFDT",
+        "`ns:Employees`.`ns1:Compensation`.`ns1:Compensation_Summary_in_Annualized_Frequency`.`ns1:Total_Base_Pay` as ANNUAL_BASE_SALARY",
+        "`ns:Employees`.`ns1:Compensation`.`ns1:Compensation_Summary_in_Annualized_Frequency`.`ns1:Currency` as ANNUAL_BASE_SALARY_CURRENCY",
+        "`ns:Employees`.`ns1:Position`.`ns1:Business_Site`.`ns1:Location_ID` as BUSINESS_UNIT_DESCR",
+        "`ns:Employees`.`ns1:Employee_Contract`.`ns1:Contract_ID` as CONTRACT_TYPE",
+        "`ns:Employees`.`ns1:Employee_Contract`.`ns1:Contract_Type` as `CONTRACT_TYPE_DESCR`",
+        "`natioId`.`ns1:Country` as `PERS_COUNTRY`",
+
+        //Estos son los de color Amarillo que si había dudas
+        "`ns:Employees`.`ns1:Worker_Status`.`ns1:Active_Status_Date` as `EMPL_STATUS_EFFDT`",
+        "`ns:Employees`.`ns1:Personal`.`ns1:Preferred_Language` as LANG_CD",
+        "`ns:Employees`.`ns1:Personal`.`ns1:Gender` as GENDER",
+        "`ns:Employees`.`ns1:Personal`.`ns1:Disability_Status` as DISABLE_TYPE_ESP",
+        "`ns:Employees`.`ns1:Personal`.`ns1:Disability_Status` as DISABLE_TYPE_ESP_DESCR",
+        "`relatedPerson`.`ns1:Dependent_ID` as EMPLID_CONYUGE",
+        "`ns:Employees`.`ns1:Worker_Status`.`ns1:Hire_Date` as COMPANY_EFFDT",
+        "`ns:Employees`.`ns1:Worker_Status`.`ns1:Original_Hire_Date` as ZING_GRUPO_DT",
+        "`ns:Employees`.`ns1:Collective_Agreement`.`ns1:Start_Date` as COLLECTIVE_YN_GLOBAL_EFFDT",
+        "`ns:Employees`.`ns1:Collective_Agreement`.`ns1:Start_Date` as COLLECTIVE_YN_LOCAL_EFFDT",
+        "`ns:Employees`.`ns1:Position`.`ns1:Organization` as TIP_DEPTID",
+        "`ns:Employees`.`ns1:Collective_Agreement`.`ns1:Collective_Agreement` as COLLECTIVE_AGREEMENT",
+        "`ns:Employees`.`ns1:Collective_Agreement`.`ns1:Collective_Agreement_Factor`.`ns1:Factor` as COLLECTIVE_AGREEMENT_FACTOR",
+        "`ns:Employees`.`ns1:Position`.`ns1:Organization` as CORP_SEGM_EFFDT",
+        "`ns:Employees`.`ns1:Worker_Status`.`ns1:First_Day_of_Work` as `MOTIVO_ALTA_HR_EFFDT`",
+        "`ns:Employees`.`ns1:Position`.`ns1:Job_Classification`.`ns1:Job_Classification_Description` as `ESCALA_JEFATURA_DT`",
+        "`ns:Employees`.`ns1:Worker_Status`.`ns1:Seniority_Date` as `TRIENIOS_DT`",
+        "`ns:Employees`.`ns1:Worker_Status`.`ns1:Seniority_Date` as `TRIENIOS_EFFDT`",
+        "`ns:Employees`.`ns1:Worker_Status`.`ns1:Seniority_Date` as `TRIENIOS_JEFATURA_DT`",
+        "`ns:Employees`.`ns1:Worker_Status`.`ns1:Seniority_Date` as `TRIENIOS_JEFATURA_EFFDT`",
+        "`ns:Employees`.`ns1:Compensation`.`ns1:Compensation_Grade` as `ESCALA_ADMIN_DT`",
+        "`ns:Employees`.`ns1:Worker_Status`.`ns1:Seniority_Date` as `ZBANK_SENIORITY_DT`",
+
       ).na.fill(" ").distinct().show()
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
