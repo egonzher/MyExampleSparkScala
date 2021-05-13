9129a8ca20fb5ace169035e594a2414551244726
egonzher <77987414+egonzher@users.noreply.github.com>
2021-05-13 16:20:41 +0200

    Nuevos cambios subidos

--- src/main/scala/com/sparkbyexamples/spark/rrhh/etl_datos_cvdatalake_worker_all_data.scala
+++ src/main/scala/com/sparkbyexamples/spark/rrhh/etl_datos_cvdatalake_worker_all_data.scala
@@ -28,34 +28,10 @@ object etl_datos_cvdatalake_worker_all_data {
     println("Imprimiendo el esquema de df_CvDatalakeWorker")
     df_CvDatalakeWorker.printSchema()
 
-/*
- |-- ns:Employees: struct (nullable = true)
- |    |-- ns1:Additional_Information: struct (nullable = true)
- |    |    |-- ns1:Company: string (nullable = true)
- |    |-- ns1:Qualifications: struct (nullable = true)
- |    |    |-- ns1:Certification_Achievement: array (nullable = true)
- |    |    |    |-- element: struct (containsNull = true)
- |    |    |    |    |-- ns1:Certification: string (nullable = true)
- |    |    |    |    |-- ns1:Certification_Country: string (nullable = true)
- |    |    |    |    |-- ns1:Certification_Issuer: string (nullable = true)
- |    |    |    |    |-- ns1:Certification_Name: string (nullable = true)
- |    |    |    |    |-- ns1:Issued_Date: string (nullable = true)
- |    |    |-- ns1:External_Job: struct (nullable = true)
- |    |    |    |-- ns1:Company: string (nullable = true)
- |    |    |    |-- ns1:End_Date: string (nullable = true)
- |    |    |    |-- ns1:Job_Reference: long (nullable = true)
- |    |    |    |-- ns1:Job_Title: string (nullable = true)
- |    |    |    |-- ns1:Location: string (nullable = true)
- |    |    |    |-- ns1:Responsibilities_And_Achievements: string (nullable = true)
- |    |    |    |-- ns1:Start_Date: string (nullable = true)
- |    |-- ns1:Summary: struct (nullable = true)
- |    |    |-- ns1:Employee_ID: long (nullable = true)
-*/
-
     val df_CvDatalakeWorkerFinal = df_CvDatalakeWorker
       .withColumn("CertAchievement", explode(col("`ns:Employees`.`ns1:Qualifications`.`ns1:Certification_Achievement`")))
       .withColumn("IDCast",col("`ns:Employees`.`ns1:Summary`.`ns1:Employee_ID`").cast("String"))
-      .withColumn("External_Job_Reference",col("`ns:Employees`.`ns1:Qualifications`.`ns1:External_Job`.`ns1:Job_Reference`").cast("String"))
+      //.withColumn("External_Job_Reference",col("`ns:Employees`.`ns1:Qualifications`.`ns1:External_Job`.`ns1:Job_Reference`").cast("String")) //2021-05-06 no aparece
       .selectExpr(
         //ID para los join
         "IDCast as Employee_ID",
@@ -64,7 +40,7 @@ object etl_datos_cvdatalake_worker_all_data {
         "`CertAchievement`.`ns1:Certification` as Certification",
         "`CertAchievement`.`ns1:Certification_Name` as Certification_Name",
         "`CertAchievement`.`ns1:Certification_Issuer` as Certification_Issuer",
-        "`CertAchievement`.`ns1:Issued_Date` as Issued_Date",
+        //"`CertAchievement`.`ns1:Issued_Date` as Issued_Date", //2021-05-06 no aparece
         //Campos pendientes de identificar
         "`ns:Employees`.`ns1:Additional_Information`.`ns1:Company` as Additional_Information_Company",
         "`ns:Employees`.`ns1:Qualifications`.`ns1:External_Job`.`ns1:Job_Title` as Job_Title",
@@ -74,7 +50,7 @@ object etl_datos_cvdatalake_worker_all_data {
         "`ns:Employees`.`ns1:Qualifications`.`ns1:External_Job`.`ns1:Responsibilities_And_Achievements` as Responsibilities_And_Achievements",
         //Campos que no están mapeados
         "`ns:Employees`.`ns1:Qualifications`.`ns1:External_Job`.`ns1:Company` as External_Job_Company",
-        "External_Job_Reference",
+        //"External_Job_Reference", //2021-05-06 no aparece
         s"'$dataDatePart' as data_date_part").na.fill(" ").distinct()
 
     df_CvDatalakeWorkerFinal.show()
