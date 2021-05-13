c8259b2bcade75d440bfddafb96e019e7c2d7145
egonzher <77987414+egonzher@users.noreply.github.com>
2021-05-13 21:41:02 +0200

    Cambiando los explode por explode_outer

--- src/main/scala/com/sparkbyexamples/spark/rrhh/etl_datos_cvdatalake_worker_all_data.scala
+++ src/main/scala/com/sparkbyexamples/spark/rrhh/etl_datos_cvdatalake_worker_all_data.scala
@@ -15,9 +15,12 @@ object etl_datos_cvdatalake_worker_all_data {
 
 
     //Trabajando con la nueva integración de cvdatalake_worker
-    val dataDatePart = "2021-05-03"
-    //val rutaCvDatalakeWorker = s"src/main/resources/rrhh/example_cv_datalake_worker/data_date_part=$dataDatePart/CvDatalake_Worker_20210505.xml"
-    val rutaCvDatalakeWorker = s"src/main/resources/rrhh/example_cv_datalake_worker/data_date_part=$dataDatePart/CvDatalake_Worker_20210506.xml"
+    //val dataDatePart = "2021-04-23"
+    //val dataDatePart = "2021-05-04"
+    //val dataDatePart = "2021-05-05"
+    val dataDatePart = "2021-05-06"
+    val rutaCvDatalakeWorker = s"src/main/resources/rrhh/example_cv_datalake_worker/data_date_part=$dataDatePart/*.xml"
+
 
 
     val df_CvDatalakeWorker = spark.read
@@ -30,55 +33,143 @@ object etl_datos_cvdatalake_worker_all_data {
     df_CvDatalakeWorker.printSchema()
 
 
-    val df_CvDatalakeWorkerFinal = df_CvDatalakeWorker
 
-      //2021-05-05 no es de tipo array por tanto se accede como struct
-      .withColumn("CertAchievement", explode_outer(col("`ns:Employees`.`ns1:Qualifications`.`ns1:Certification_Achievement`")))
-      //.withColumn("CertAchievement", col("`ns:Employees`.`ns1:Qualifications`.`ns1:Certification_Achievement`"))
+    //////////////////////////////////////////////////////////////////////////////
 
-      .withColumn("IDCast",col("`ns:Employees`.`ns1:Summary`.`ns1:Employee_ID`").cast("String"))
+    /*
 
-      //2021-05-06 no aparece
-      //.withColumn("External_Job_Reference",col("`ns:Employees`.`ns1:Qualifications`.`ns1:External_Job`.`ns1:Job_Reference`").cast("String"))
+      df_CvDatalakeWorkerFinal = df_CvDatalakeWorker
+        .withColumn("CertAchievement", explode_outer(col("`ns:Employees`.`ns1:Qualifications`.`ns1:Certification_Achievement`")))
+        .withColumn("IDCast",col("`ns:Employees`.`ns1:Summary`.`ns1:Employee_ID`").cast("String"))
+        .withColumn("External_Job_Reference",col("`ns:Employees`.`ns1:Qualifications`.`ns1:External_Job`.`ns1:Job_Reference`").cast("String"))
+        .selectExpr(
+          "IDCast as Employee_ID",
+          "`CertAchievement`.`ns1:Certification_Country` as Certification_Country",
+          "`CertAchievement`.`ns1:Certification` as Certification",
+          "`CertAchievement`.`ns1:Certification_Name` as Certification_Name",
+          "`CertAchievement`.`ns1:Certification_Issuer` as Certification_Issuer",
+          "`CertAchievement`.`ns1:Issued_Date` as Issued_Date",
+          "`ns:Employees`.`ns1:Additional_Information`.`ns1:Company` as Additional_Information_Company",
+          "`ns:Employees`.`ns1:Qualifications`.`ns1:External_Job`.`ns1:Job_Title` as Job_Title",
+          "`ns:Employees`.`ns1:Qualifications`.`ns1:External_Job`.`ns1:Start_Date` as Start_Date",
+          "`ns:Employees`.`ns1:Qualifications`.`ns1:External_Job`.`ns1:End_Date` as End_Date",
+          "`ns:Employees`.`ns1:Qualifications`.`ns1:External_Job`.`ns1:Location` as External_Job_Location",
+          "`ns:Employees`.`ns1:Qualifications`.`ns1:External_Job`.`ns1:Responsibilities_And_Achievements` as Responsibilities_And_Achievements",
+          "`ns:Employees`.`ns1:Qualifications`.`ns1:External_Job`.`ns1:Company` as External_Job_Company",
+          "External_Job_Reference",
+          s"'$dataDatePart' as data_date_part").na.fill(" ").distinct()
 
-      .selectExpr(
 
-        "IDCast as Employee_ID",
+     */
 
-        //2021-05-05 no aparece
-        "`CertAchievement`.`ns1:Certification_Country` as Certification_Country",
-        //"'' as Certification_Country",
+    //////////////////////////////////////////////////////////////////////////////
 
-        "`CertAchievement`.`ns1:Certification` as Certification",
-        "`CertAchievement`.`ns1:Certification_Name` as Certification_Name",
-        "`CertAchievement`.`ns1:Certification_Issuer` as Certification_Issuer",
 
-        //2021-05-06 no aparece
-        //2021-05-05 no aparece
-        //"`CertAchievement`.`ns1:Issued_Date` as Issued_Date",
-        "'' as Issued_Date",
 
-        "`ns:Employees`.`ns1:Additional_Information`.`ns1:Company` as Additional_Information_Company",
-        "`ns:Employees`.`ns1:Qualifications`.`ns1:External_Job`.`ns1:Job_Title` as Job_Title",
-        "`ns:Employees`.`ns1:Qualifications`.`ns1:External_Job`.`ns1:Start_Date` as Start_Date",
 
-        //2021-05-05 no aparece
-        "`ns:Employees`.`ns1:Qualifications`.`ns1:External_Job`.`ns1:End_Date` as End_Date",
-        //"'' as End_Date",
+    var df_CvDatalakeWorkerFinal = df_CvDatalakeWorker
 
-        "`ns:Employees`.`ns1:Qualifications`.`ns1:External_Job`.`ns1:Location` as External_Job_Location",
+    if (dataDatePart == "2021-05-06"){
 
-        //2021-05-05 no aparece
-        "`ns:Employees`.`ns1:Qualifications`.`ns1:External_Job`.`ns1:Responsibilities_And_Achievements` as Responsibilities_And_Achievements",
-        //"'' as Responsibilities_And_Achievements",
+      df_CvDatalakeWorkerFinal = df_CvDatalakeWorker
+        .withColumn("CertAchievement", explode_outer(col("`ns:Employees`.`ns1:Qualifications`.`ns1:Certification_Achievement`")))
+        .withColumn("IDCast",col("`ns:Employees`.`ns1:Summary`.`ns1:Employee_ID`").cast("String"))
+        .selectExpr(
+          "IDCast as Employee_ID",
+          "`CertAchievement`.`ns1:Certification_Country` as Certification_Country",
+          "`CertAchievement`.`ns1:Certification` as Certification",
+          "`CertAchievement`.`ns1:Certification_Name` as Certification_Name",
+          "`CertAchievement`.`ns1:Certification_Issuer` as Certification_Issuer",
+          "'' as Issued_Date",
+          "`ns:Employees`.`ns1:Additional_Information`.`ns1:Company` as Additional_Information_Company",
+          "`ns:Employees`.`ns1:Qualifications`.`ns1:External_Job`.`ns1:Job_Title` as Job_Title",
+          "`ns:Employees`.`ns1:Qualifications`.`ns1:External_Job`.`ns1:Start_Date` as Start_Date",
+          "`ns:Employees`.`ns1:Qualifications`.`ns1:External_Job`.`ns1:End_Date` as End_Date",
+          "`ns:Employees`.`ns1:Qualifications`.`ns1:External_Job`.`ns1:Location` as External_Job_Location",
+          "`ns:Employees`.`ns1:Qualifications`.`ns1:External_Job`.`ns1:Responsibilities_And_Achievements` as Responsibilities_And_Achievements",
+          "`ns:Employees`.`ns1:Qualifications`.`ns1:External_Job`.`ns1:Company` as External_Job_Company",
+          "'' as External_Job_Reference",
+          s"'$dataDatePart' as data_date_part").na.fill(" ").distinct()
 
-        "`ns:Employees`.`ns1:Qualifications`.`ns1:External_Job`.`ns1:Company` as External_Job_Company",
+    }
 
-        //2021-05-06 no aparece
-        //"External_Job_Reference",
-        "'' as External_Job_Reference",
+    else if (dataDatePart == "2021-05-05"){
 
-        s"'$dataDatePart' as data_date_part").na.fill(" ").distinct()
+      df_CvDatalakeWorkerFinal = df_CvDatalakeWorker
+        .withColumn("CertAchievement", col("`ns:Employees`.`ns1:Qualifications`.`ns1:Certification_Achievement`"))
+        .withColumn("IDCast",col("`ns:Employees`.`ns1:Summary`.`ns1:Employee_ID`").cast("String"))
+        .withColumn("External_Job_Reference",col("`ns:Employees`.`ns1:Qualifications`.`ns1:External_Job`.`ns1:Job_Reference`").cast("String"))
+        .selectExpr(
+          "IDCast as Employee_ID",
+          "'' as Certification_Country",
+          "`CertAchievement`.`ns1:Certification` as Certification",
+          "`CertAchievement`.`ns1:Certification_Name` as Certification_Name",
+          "`CertAchievement`.`ns1:Certification_Issuer` as Certification_Issuer",
+          "'' as Issued_Date",
+          "`ns:Employees`.`ns1:Additional_Information`.`ns1:Company` as Additional_Information_Company",
+          "`ns:Employees`.`ns1:Qualifications`.`ns1:External_Job`.`ns1:Job_Title` as Job_Title",
+          "`ns:Employees`.`ns1:Qualifications`.`ns1:External_Job`.`ns1:Start_Date` as Start_Date",
+          "'' as End_Date",
+          "`ns:Employees`.`ns1:Qualifications`.`ns1:External_Job`.`ns1:Location` as External_Job_Location",
+          "'' as Responsibilities_And_Achievements",
+          "`ns:Employees`.`ns1:Qualifications`.`ns1:External_Job`.`ns1:Company` as External_Job_Company",
+          "External_Job_Reference",
+          s"'$dataDatePart' as data_date_part").na.fill(" ").distinct()
+
+    }
+
+    else if (dataDatePart == "2021-05-04"){
+
+      df_CvDatalakeWorkerFinal = df_CvDatalakeWorker
+        .withColumn("CertAchievement", explode_outer(col("`ns:Employees`.`ns1:Qualifications`.`ns1:Certification_Achievement`")))
+        .withColumn("IDCast",col("`ns:Employees`.`ns1:Summary`.`ns1:Employee_ID`").cast("String"))
+        .selectExpr(
+          "IDCast as Employee_ID",
+          "`CertAchievement`.`ns1:Certification_Country` as Certification_Country",
+          "`CertAchievement`.`ns1:Certification` as Certification",
+          "`CertAchievement`.`ns1:Certification_Name` as Certification_Name",
+          "`CertAchievement`.`ns1:Certification_Issuer` as Certification_Issuer",
+          "'' as Issued_Date",
+          "`ns:Employees`.`ns1:Additional_Information`.`ns1:Company` as Additional_Information_Company",
+          "`ns:Employees`.`ns1:Qualifications`.`ns1:External_Job`.`ns1:Job_Title` as Job_Title",
+          "`ns:Employees`.`ns1:Qualifications`.`ns1:External_Job`.`ns1:Start_Date` as Start_Date",
+          "`ns:Employees`.`ns1:Qualifications`.`ns1:External_Job`.`ns1:End_Date` as End_Date",
+          "`ns:Employees`.`ns1:Qualifications`.`ns1:External_Job`.`ns1:Location` as External_Job_Location",
+          "`ns:Employees`.`ns1:Qualifications`.`ns1:External_Job`.`ns1:Responsibilities_And_Achievements` as Responsibilities_And_Achievements",
+          "'' as External_Job_Company",
+          "'' as External_Job_Reference",
+          s"'$dataDatePart' as data_date_part").na.fill(" ").distinct()
+
+    }
+
+    else if (dataDatePart == "2021-04-23"){
+
+      df_CvDatalakeWorkerFinal = df_CvDatalakeWorker
+        .withColumn("CertAchievement", col("`ns:Employees`.`ns1:Qualifications`.`ns1:Certification_Achievement`"))
+        .withColumn("IDCast",col("`ns:Employees`.`ns1:Summary`.`ns1:Employee_ID`").cast("String"))
+        .selectExpr(
+          "IDCast as Employee_ID",
+          "`CertAchievement`.`ns1:Certification_Country` as Certification_Country",
+          "`CertAchievement`.`ns1:Certification` as Certification",
+          "`CertAchievement`.`ns1:Certification_Name` as Certification_Name",
+          "`CertAchievement`.`ns1:Certification_Issuer` as Certification_Issuer",
+          "`CertAchievement`.`ns1:Issued_Date` as Issued_Date",
+          "'' as Additional_Information_Company",
+          "'' as Job_Title",
+          "'' as Start_Date",
+          "'' as End_Date",
+          "'' as External_Job_Location",
+          "'' as Responsibilities_And_Achievements",
+          "'' as External_Job_Company",
+          "'' as External_Job_Reference",
+          s"'$dataDatePart' as data_date_part").na.fill(" ").distinct()
+
+    }
+
+    else {
+      println("La partición insertada no coincide on las que existen actualmente")
+    }
+
 
     df_CvDatalakeWorkerFinal.show()
 
